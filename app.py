3a4c247c1f691f8c9c6737fc56beec4d4a9923c1
hacker-niki <nktsenko@gmail.com>
2023-03-28 18:38:13 +0300

    ver 0.2

--- app.py
+++ app.py
@@ -1,49 +1,49 @@
-import speech_recognition as sr
 import pyttsx3
+import speech_recognition as sr
+import datetime
 
-# создаем объект для распознавания речи
-r = sr.Recognizer()
-
-# создаем объект для синтеза речи
+# Создаем объект для синтеза речи
 engine = pyttsx3.init()
-engine.say("I will speak this text")
 
-# задаем голос для синтеза речи
+# Устанавливаем голос для синтеза речи
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)  # выбираем женский голос
+engine.setProperty('voice', voices[0].id)
 
-
-# функция для синтеза речи
-def speak(t):
-    engine.say(t)
+# Функция для воспроизведения речи
+def speak(text):
+    engine.say(text)
     engine.runAndWait()
 
-
-# функция для распознавания речи
+# Функция для распознавания речи
 def recognize_speech():
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Скажите что-нибудь...")
+        print("Говорите...")
         audio = r.listen(source)
-
         try:
-            text = r.recognize_google(audio, language="ru-RU")
-            print("Вы сказали: " + text)
+            text = r.recognize_google(audio, language='ru-RU')
+            print("Вы сказали: {}".format(text))
             return text
-        except sr.UnknownValueError:
-            print("Речь не распознана")
-        except sr.RequestError as e:
-            print("Ошибка сервиса распознавания речи; {0}".format(e))
+        except:
+            print("Не удалось распознать речь")
+            return ""
 
+# Функция для выполнения команд
+def process_command(command):
+    if "привет" in command.lower():
+        speak("Привет! Как дела?")
+    elif "как дела" in command.lower():
+        speak("У меня все хорошо, спасибо. А у тебя?")
+    elif "который час" in command.lower():
+        now = datetime.datetime.now()
+        speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
+    elif "стоп" in command.lower():
+        speak("До свидания!")
+        return True
+    return False
 
-# основной код для голосового ассистента
-if __name__ == '__main__':
-    # получаем речь от пользователя
-    text = recognize_speech()
-
-    # обрабатываем команду и генерируем ответ
-    if "привет" in text:
-        speak("Здравствуйте, чем я могу вам помочь?")
-    elif "как дела" in text:
-        speak("У меня всё хорошо, спасибо за ваш интерес!")
-    else:
-        speak("Извините, я не поняла вашей команды.")
+# Главный цикл программы
+while True:
+    command = recognize_speech()
+    if process_command(command):
+        break
