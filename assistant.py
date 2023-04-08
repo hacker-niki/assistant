423900370b887b50a54d23e8a50958f0de194e82
hacker-niki <nktsenko@gmail.com>
2023-04-08 14:26:13 +0600

    TextHandler

--- assistant.py
+++ assistant.py
@@ -1,8 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
-import datetime
-from functions import extract_city
-from functions import get_weather
+import TextHandler
 
 
 class Assistant:
@@ -10,14 +8,12 @@ class Assistant:
         # Создаем объект для синтеза речи
         self.engine = pyttsx3.init()
 
-        # Устанавливаем голос для синтеза речи
         voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', voices[0].id)
+        self.engine.setProperty('voice', voices[2].id)
 
         self.speak("Здарова, Меченый!")
 
     def run(self):
-        # Главный цикл программы
         while True:
             command = self.recognize_speech()
             if self.process_command(command):
@@ -37,7 +33,7 @@ class Assistant:
             audio = r.listen(source)
             try:
                 text = r.recognize_google(audio, language='ru-RU')
-                print("Вы сказали: {}".format(text))
+                print(f"Вы сказали: {format(text)}")
                 return text
             except:
                 print("Не удалось распознать речь")
@@ -45,18 +41,5 @@ class Assistant:
 
     # Функция для выполнения команд
     def process_command(self, command):
-        if "привет" in command.lower():
-            self.speak("Хабар принес?")
-        elif "как дела" in command.lower():
-            self.speak("Можно и передохнуть маленько")
-        elif "который час" in command.lower():
-            now = datetime.datetime.now()
-            self.speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-        elif extract_city(command) is not None:
-            self.speak(get_weather(extract_city(command)))
-            # print("погода")
-        elif "стоп" in command.lower():
-            self.speak("Ну, удачной охоты, сталкер.")
-            return True
-        self.speak("Иди своей дорогой, сталкер!")
+
         return False
--- functions.py
+++ functions.py
@@ -1,28 +1,23 @@
+import datetime
 import re
 import requests
-# from translate import Translator
 from googletrans import Translator, constants
 
 
-def extract_city(command: str):
-    # задаем регулярное выражение для поиска города
+def extract_city_function(command: str):
     pattern = r"погода в ([\w\s]+)"
-    # применяем регулярное выражение к команде пользователя
     match = re.search(pattern, command.lower())
-    # если найдено совпадение, возвращаем название города
     if match:
         return match.group(1)
-    # если совпадений нет, возвращаем None
     else:
         return None
 
 
-def get_weather(city: str):
+def get_weather_function(city: str):
     translator = Translator()
-    result = translator.translate("город", src='ru', dest='en')
+    result = translator.translate(city, src='ru', dest='en')
     api_key = "d3b9ddd02bf307000417e311a213a7f4"  # замените на свой API ключ OpenWeatherMap
     base_url = f"https://api.openweathermap.org/data/2.5/weather?q={result.text}&appid={api_key}&units=metric"
-    # print(translator.translate(city).text)
     response = requests.get(base_url)
     if response.status_code == 200:
         data = response.json()
@@ -35,3 +30,20 @@ def get_weather(city: str):
             dest="ru").text
     else:
         return "Извините, я не смог получить информацию о погоде в этом городе."
+
+
+def hello_function() -> str:
+    return "Хабар принес?"
+
+
+def doing_function() -> str:
+    return "Можно и передохнуть маленько"
+
+
+def time_function() -> str:
+    now = datetime.datetime.now()
+    return "Сейчас " + str(now.hour) + ":" + str(now.minute)
+
+
+def stop_function() -> str:
+    return "Ну, удачной охоты, сталкер."
