5be0b3f55c5a4724988d0da2edcb157e3bef1ad4
hacker-niki <nktsenko@gmail.com>
2023-04-09 16:33:53 +0600

    small optimizations

--- assistant.py
+++ assistant.py
@@ -8,8 +8,8 @@ class Assistant:
         # Создаем объект для синтеза речи
         self.engine = pyttsx3.init()
         self.handler = textHandler.TextHandler()
-        self.voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', self.voices[2].id)
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[2].id)
 
         self.speak("Здарова, Меченый!")
 
@@ -25,7 +25,6 @@ class Assistant:
         self.engine.runAndWait()
 
     # Функция для распознавания речи
-
     def recognize_speech(self):
         r = sr.Recognizer()
         with sr.Microphone() as source:
