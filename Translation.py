8398c74db180e59b757e37ca24a22b33d83406d6
Veronika Yarmak <125794463+yarmachoo@users.noreply.github.com>
2023-04-09 21:17:40 +0300

    Realise class Translation.py (as Maxim)

--- Translation.py
+++ Translation.py
@@ -1,2 +1,22 @@
-#предлагаю эту часть кода сделать, как у максима,
-# но поищу еще варианты
+#предлагаю эту часть кода сделать, как у максима, но поищу еще варианты
+import json
+from assistant import Assistant
+class Translation:
+    """
+    Получение вшитого в приложение перевода строк для создания мультиязычного ассистента
+    """
+    with open("translations.json", "r", encoding="UTF-8") as file:
+        translations = json.load(file)
+
+    def get(self, text: str):
+        """
+        Получение перевода строки из файла на нужный язык (по его коду)
+        :param text: текст, который требуется перевести
+        :return: вшитый в приложение перевод текста
+        """
+        if text in self.translations:
+            return self.translations[text][assistant.speech_language]
+        else:
+            # в случае отсутствия перевода происходит вывод сообщения об этом в логах и возврат исходного текста
+            print(colored("Not translated phrase: {}".format(text), "red"))
+            return text
