e5e8bbb38e75f35194bf57f3f1cc774fd7730da7
Paskushka <112726412+Paskushka@users.noreply.github.com>
2023-04-09 23:00:08 +0300

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,29 +1,24 @@
 import pyttsx3
 import speech_recognition as sr
 import textHandler
+import TextToVoice
 
 
 class Assistant:
     def __init__(self):
         # Создаем объект для синтеза речи
-        self.engine = pyttsx3.init()
+        
+        self.speak = TextToVoice.TextToVoice()
         self.handler = textHandler.TextHandler()
-        self.voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', self.voices[2].id)
 
-        self.speak("Здарова, Меченый!")
+        self.speak.text_to_voice("Здарова, Меченый!")
 
     def run(self):
         while True:
             command = self.recognize_speech()
             if not self.process_command(command):
                 break
-
-    # Функция для воспроизведения речи
-    def speak(self, text):
-        self.engine.say(text)
-        self.engine.runAndWait()
-
+                
     # Функция для распознавания речи
 
     def recognize_speech(self):
@@ -42,5 +37,5 @@ class Assistant:
     # Функция для выполнения команд
     def process_command(self, command) -> bool:
         result = self.handler.map_string_to_function(input_string=command)
-        self.speak(result[1])
+        self.speak.text_to_voice(result[1])
         return result[0]
