24ac65010ca7393e04da1c9e5f3aa96dd46cd1aa
hacker-niki <nktsenko@gmail.com>
2023-04-11 19:21:13 +0600

    hotfix

--- audioProcessor.py
+++ audioProcessor.py
@@ -2,7 +2,6 @@ import speech_recognition as sr
 import pyttsx3
 
 
-<<<<<<< HEAD:AudioProcessor.py
 class AudioProcessor():
     
     def __init__(self):
@@ -14,20 +13,7 @@ class AudioProcessor():
 
         try:
             text = self.recognizer.recognize_google(audio, language="ru-Ru")
-=======
-class AudioProcessor:
-    def __init__(self):
-        self.recognizer = sr.Recognizer()
-        self.engine = pyttsx3.init()
-        self.voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice', self.voices[2].id)
-
-    def audio_to_text(self, audio):
-        # функция перевода текстa в аудио
-        try:
-            text = self.recognizer.recognize_google(audio, language="ru-Ru").lower()
->>>>>>> 4f883dea4867365920cf61f2d1053ec5f962d0ff:audioProcessor.py
-            return text
+            return str(texts)
         except sr.RequestError:
             print("Что-то с интернетом(( Проверьте подключение, пожалуйста")
             return ""
