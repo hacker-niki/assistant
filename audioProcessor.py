908b75d90294bbf4d6d9e2d1110f7bb6486490f0
Veronika Yarmak <125794463+yarmachoo@users.noreply.github.com>
2023-04-25 13:46:53 +0300

    add func use_offline_recognition

--- audioProcessor.py
+++ audioProcessor.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import pyttsx3
-
+import vosk
+import os
+import wave
+import json
+import traceback
 
 class AudioProcessor():
     
@@ -8,8 +12,62 @@ class AudioProcessor():
         self.recognizer = sr.Recognizer()
         self.engine = pyttsx3.init()
 
+    def use_offline_recognition(language):
+        recognized_data = ""
+        try:
+            # проверка наличия модели на нужном языке в каталоге приложения
+
+            # Необходимо ввести путь к местоположению модуля распознавания воск!!!!!!1
+            model_path = ""
+
+            if (language == "russian"):
+                model_path = "models/vosk-model-small-ru-0.22"  # путь к файлу модели
+            elif (language == "english"):
+                model_path = "models/vosk-model-en-us-0.22-lgraph"  # путь к файлу модели
+
+            if not os.path.exists(model_path):
+                print("Please download the model from:\n"
+                      "https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+                exit(1)
+
+            # анализ записанного в микрофон аудио (чтобы избежать повторов фразы)
+            wave_audio_file = wave.open("microphone-results.wav", "rb")
+            model = vosk.Model(model_path)
+            rec = vosk.KaldiRecognizer(model, wave_audio_file.getframerate())
+
+            data = wave_audio_file.readframes(wave_audio_file.getnframes())
+
+            last_n = True
+
+            while last_n:
+                last_n = False
+                if len(data) > 0:
+                    if rec.AcceptWaveform(data):
+                        # получение данных распознанного текста из JSON-строки (чтобы можно было выдать по ней ответ)
+                        res = json.loads(rec.Result())
+
+                        if res['text'] != '':
+                            recognized_data += f" {res['text']}"
+                            last_n = False
+                        elif not last_n:
+                            recognized_data += '\n'
+                            last_n = True
+
+            res = json.loads(rec.FinalResult())
+            recognized_data += f" {res['text']}"
+
+        except:
+            traceback.print_exc()
+            print("Sorry, speech service is unavailable. Try again later")
+
+        return recognized_data
+
     def audio_to_text(self, audio):
-        # функция перевода текстa в аудио
+        # функция перевода аудио в текст
+
+        # запишем аудио в файл для перевода в текст через vosk
+        with open("microphone-results.wav", "wb") as file:
+            file.write(audio.get_wav_data())
 
         try:
             text = self.recognizer.recognize_google(audio, language="ru-Ru")
@@ -25,3 +83,4 @@ class AudioProcessor():
         # функция воспроизведения текста
         self.engine.say(text)
         self.engine.runAndWait()
+
